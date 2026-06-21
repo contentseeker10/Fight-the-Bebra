8de2df51eefa48769d7c886301144be237c729fa
contentseeker10 <appelsene.design@gmail.com>
2026-06-21 22:19:50 +0300

    implement lobby service logic

--- server/src/main/java/dev/contentseeker10/services/LobbyService.java
+++ server/src/main/java/dev/contentseeker10/services/LobbyService.java
@@ -2,6 +2,9 @@ package dev.contentseeker10.services;
 
 import dev.contentseeker10.model.Lobby;
 import dev.contentseeker10.model.User;
+import dev.contentseeker10.model.type.LobbyType;
+import dev.contentseeker10.model.type.UserType;
+import net.bytebuddy.utility.RandomString;
 
 import java.util.Map;
 import java.util.concurrent.ConcurrentHashMap;
@@ -14,20 +17,66 @@ public class LobbyService {
 
     private final Map<String, Lobby> activeLobbies = new ConcurrentHashMap<>();
 
-    public void createLobby(User creator) {
+    // TODO: Make DTO for Lobby Response data and switch return type to it instead of String
 
+    public String createLobby(User creator) {
+        creator.setType(UserType.ADMIN);
+        String code;
+        Lobby lobby;
+        do {
+            code = generateCode();
+            lobby = new Lobby(code, creator);
+        } while (activeLobbies.putIfAbsent(code, lobby) != null);
+        return code;
     }
 
-    public void enterLobby(User user) {
-
+    public String enterLobby(String lobbyCode, User user) {
+        Lobby lobby = activeLobbies.get(lobbyCode);
+        if (lobby == null) {
+            return "Lobby not found";
+        }
+        synchronized (lobby) {
+            if (lobby.getGuest() != null) {
+                return "Room is full";
+            }
+            if (lobby.getType() != LobbyType.WAITING) {
+                return "Game already started";
+            }
+            user.setType(UserType.GUEST);
+            lobby.setGuest(user);
+            return "Success";
+        }
     }
 
-    public void leaveLobby(User user) {
-
+    public String leaveLobby(String lobbyCode, User user) {
+        Lobby lobby = activeLobbies.get(lobbyCode);
+        if (lobby == null || lobby.getAdmin() == null) {
+            return "Bad Request";
+        }
+        synchronized (lobby) {
+            UserType userType = user.getType();
+            if (userType == UserType.ADMIN) {
+                lobby.getAdmin().setType(UserType.NONE);
+                if (lobby.getGuest() != null) {
+                    lobby.getGuest().setType(UserType.ADMIN);
+                    lobby.setAdmin(lobby.getGuest());
+                    lobby.setGuest(null);
+                } else {
+                    lobby.setAdmin(null);
+                    activeLobbies.remove(lobbyCode);
+                }
+            } else if (userType == UserType.GUEST) {
+                lobby.getGuest().setType(UserType.NONE);
+                lobby.setGuest(null);
+            } else {
+                return "Bad Request";
+            }
+            return "Success";
+        }
     }
 
     private static String generateCode() {
-
+        return RandomString.make(5).toUpperCase();
     }
 
 }
