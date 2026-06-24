a46887f538213a7dfb52284a8b2840c99d4dd365
contentseeker10 <appelsene.design@gmail.com>
2026-06-24 14:17:29 +0300

    add leave call to network

--- client/ui/menus/lobby/room_create/room_create.gd
+++ client/ui/menus/lobby/room_create/room_create.gd
@@ -36,7 +36,7 @@ func _on_start_button_pressed() -> void:
 
 
 func _on_exit_button_pressed() -> void:
-	# TODO: Call to Java client over NetworkManager (ROOM DISMISSED)
+	EventManager.leave_lobby_requested.emit(LobbyManager.code)
 	get_tree().change_scene_to_file("res://ui/menus/lobby/lobby.tscn")
 
 
